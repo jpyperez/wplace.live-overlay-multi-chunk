43537b7022ee553d76d87afbea9dffc1769c8346
Zary <57652636+ZaryImortal@users.noreply.github.com>
2025-09-17 19:16:22 -0300

    Update imagens.js

--- imagens.js
+++ imagens.js
@@ -4,5 +4,6 @@
   { "url": "https://i.imgur.com/aGKmeNy.png", "chunk": [762, 1165], "coords": [105, 521] },
   { "url": "https://i.imgur.com/V8BeVqy.png", "chunk": [754, 1160], "coords": [795, 265] },
   { "url": "https://i.imgur.com/FwZHDQx.png", "chunk": [762, 1179], "coords": [866, 192] },
-  { "url": "https://i.imgur.com/xz30He0.png", "chunk": [755, 1162], "coords": [551, 838] }
+  { "url": "https://i.imgur.com/xz30He0.png", "chunk": [755, 1162], "coords": [551, 838] },
+  { "url": "https://i.imgur.com/dnqVhmq.png", "chunk": [756, 1162], "coords": [885, 661] }
 ]

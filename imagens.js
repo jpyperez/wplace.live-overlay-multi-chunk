d3d82f15d820d7ca15685daee270c47e5f5f475d
Zary <57652636+ZaryImortal@users.noreply.github.com>
2025-09-22 01:43:31 -0300

    Update imagens.js

--- imagens.js
+++ imagens.js
@@ -1,10 +1,11 @@
 [
   { "url": "https://i.imgur.com/o9Eyrwo.png", "chunk": [757, 1162], "coords": [79, 232] },
   { "url": "https://i.imgur.com/oI1HPYI.png", "chunk": [761, 1158], "coords": [713, 699] },
-  { "url": "https://i.imgur.com/aGKmeNy.png", "chunk": [762, 1165], "coords": [105, 521] },
   { "url": "https://i.imgur.com/V8BeVqy.png", "chunk": [754, 1160], "coords": [795, 265] },
+  { "url": "https://i.imgur.com/aGKmeNy.png", "chunk": [762, 1165], "coords": [105, 521] },
   { "url": "https://i.imgur.com/FwZHDQx.png", "chunk": [762, 1179], "coords": [866, 192] },
   { "url": "https://i.imgur.com/xz30He0.png", "chunk": [755, 1162], "coords": [551, 838] },
+  { "url": "https://i.imgur.com/0Vi5q8y.png", "chunk": [755, 1162], "coords": [578, 595] },
   { "url": "https://i.imgur.com/dnqVhmq.png", "chunk": [756, 1162], "coords": [885, 661] },
   { "url": "https://i.imgur.com/JfwPr8j.png", "chunk": [756, 1162], "coords": [602, 11] }
 ]

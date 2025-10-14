90a9c76cc6457200830766ce406f2d20f2b9bf75
Jose Pedro <jpyperez@outlook.com>
2025-10-14 18:12:24 +0100

    Update imagens.js

--- imagens.js
+++ imagens.js
@@ -1,13 +1,3 @@
 [
-  { "url": "https://i.imgur.com/TFj8JZ8.png", "chunk": [757, 1162], "coords": [79, 232] },
-  { "url": "https://i.imgur.com/oI1HPYI.png", "chunk": [761, 1158], "coords": [713, 699] },
-  { "url": "https://i.imgur.com/LexZIZ7.png", "chunk": [754, 1156], "coords": [195, 465] },
-  { "url": "https://i.imgur.com/83ZQmU6.png", "chunk": [778, 1155], "coords": [137, 323] },
-  { "url": "https://i.imgur.com/aGKmeNy.png", "chunk": [762, 1165], "coords": [105, 521] },
-  { "url": "https://i.imgur.com/FwZHDQx.png", "chunk": [762, 1179], "coords": [866, 192] },
-  { "url": "https://i.imgur.com/xz30He0.png", "chunk": [755, 1162], "coords": [551, 838] },
-  { "url": "https://i.imgur.com/rEp12Wz.png", "chunk": [757, 1162], "coords": [102, 901] },
-  { "url": "https://i.imgur.com/dnqVhmq.png", "chunk": [756, 1162], "coords": [885, 661] },
-  { "url": "https://i.imgur.com/fqeR2Vo.png", "chunk": [756, 1161], "coords": [602, 942] },
-  { "url": "https://i.imgur.com/A6oUTlJ.png", "chunk": [757, 1162], "coords": [480, 553] }
+  { "url": "https://imgur.com/S30H1Qi", "chunk": [979, 765], "coords": [232, 406] }
 ]

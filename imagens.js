5373bedbbac723919cbd981598274fd8ba2cb537
Jose Pedro <jpyperez@outlook.com>
2025-10-17 21:09:23 +0100

    Update imagens.js

--- imagens.js
+++ imagens.js
@@ -1,4 +1,4 @@
 [
   { "url": "https://i.imgur.com/S30H1Qi.png", "chunk": [979, 765], "coords": [232, 406] },
-  { "url": "https://i.imgur.com/XfDDV5X.png", "chunk": [979, 765], "coords": [232, 406] }
+  { "url": "https://i.imgur.com/qGGVnbO.png", "chunk": [979, 765], "coords": [232, 406] }
 ]
